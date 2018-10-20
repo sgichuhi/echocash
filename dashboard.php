8ba1f65890cb568682771af7f390bde9bd96379c
Sarah Gichuhi <sgichuhi@outlook.com>
2018-10-20 19:32:06 -0400

    fixed dashboard, navigation, and updated database connection script.

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,12 @@
 <?php
 $page_title = "Dashboard";
-include_once $_SERVER['DOCUMENT_ROOT'] . "/web-assets/tpl/app_header.php";
-include_once $_SERVER['DOCUMENT_ROOT'] . "/web-assets/tpl/app_nav.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/echocash/web-assets/tpl/app_header.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/echocash/web-assets/tpl/app_nav.php";
 
 
 echo "Hello World";
 
 
 
-include_once $_SERVER['DOCUMENT_ROOT'] . "/web-assets/tpl/app_footer.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/echocash/web-assets/tpl/app_footer.php";
 ?>
--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $dbh = new mysqli('localhost','root','','echocash');
 
 if(!$dbh->error){
-    echo "database connected successfully";
+    echo "database connected successfully (ignore, testing...)";
 } else {
     echo "it didn't work.";
 }
